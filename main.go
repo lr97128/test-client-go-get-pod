958a1c319ef53dd2257f8ffd59ded10424ff99cf
liurui <lr97128@163.com>
2024-05-04 13:07:39 +0800

    create samples for this test project

--- main.go
+++ main.go
@@ -15,8 +15,7 @@ import (
 )
 
 const (
-	ns      = "default"
-	podName = "nginx-test-84b487c7d9-dhkfs"
+	ns = "default"
 )
 
 func main() {
@@ -40,25 +39,31 @@ func main() {
 	if err != nil {
 		log.Panic(err)
 	}
-	pod, err := clientSet.CoreV1().Pods(ns).Get(ctx, podName, metav1.GetOptions{})
+	pods, err := clientSet.CoreV1().Pods(ns).List(ctx, metav1.ListOptions{})
 	if err != nil {
 		log.Panic(err)
 	}
-	fmt.Printf("HostIP is: %s\n", pod.Status.HostIP)
-	for index, ip := range pod.Status.PodIPs {
-		fmt.Printf("index is: %d, ip is: %s\n", index, ip.IP)
-	}
-	fmt.Printf("pod ip is: %s\n", pod.Status.PodIP)
-	var rsName string
-	for index, ownerReference := range pod.ObjectMeta.GetOwnerReferences() {
-		rsName = ownerReference.Name
-		fmt.Printf("pod's index: %d, ownerReferenceKind: %s, ownerReferenceName: %s\n", index, ownerReference.Kind, rsName)
-	}
-	rs, err := clientSet.AppsV1().ReplicaSets(ns).Get(ctx, rsName, metav1.GetOptions{})
-	if err != nil {
-		log.Panic(err)
-	}
-	for index, ownerReference := range rs.ObjectMeta.GetOwnerReferences() {
-		fmt.Printf("ReplicaSet's index: %d, ownerReferenceKind: %s, ownerReferenceName: %s\n", index, ownerReference.Kind, ownerReference.Name)
+	for _, pod := range pods.Items {
+
+		fmt.Printf("HostIP is: %s\n", pod.Status.HostIP)
+		for _, initStatuts := range pod.Status.InitContainerStatuses {
+			fmt.Printf("InitContainerStatus is: %s\n", initStatuts.State.Terminated.Reason)
+		}
+		for index, ip := range pod.Status.PodIPs {
+			fmt.Printf("index is: %d, ip is: %s\n", index, ip.IP)
+		}
+		fmt.Printf("pod ip is: %s\n", pod.Status.PodIP)
+		var rsName string
+		for index, ownerReference := range pod.ObjectMeta.GetOwnerReferences() {
+			rsName = ownerReference.Name
+			fmt.Printf("pod's index: %d, ownerReferenceKind: %s, ownerReferenceName: %s\n", index, ownerReference.Kind, rsName)
+		}
+		rs, err := clientSet.AppsV1().ReplicaSets(ns).Get(ctx, rsName, metav1.GetOptions{})
+		if err != nil {
+			log.Panic(err)
+		}
+		for index, ownerReference := range rs.ObjectMeta.GetOwnerReferences() {
+			fmt.Printf("ReplicaSet's index: %d, ownerReferenceKind: %s, ownerReferenceName: %s\n", index, ownerReference.Kind, ownerReference.Name)
+		}
 	}
 }
